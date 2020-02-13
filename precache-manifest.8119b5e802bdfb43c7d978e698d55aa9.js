d0bce4899648cc55e52a50fdc898914c8254b240
Júlio César Lima <juliocesarlima19@gmail.com>
2020-02-12 23:55:05 -0300

    Updates

---
+++ precache-manifest.8119b5e802bdfb43c7d978e698d55aa9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f6b3de5fe38ae016de602c4c06463d4",
+    "url": "/curriculo-vetta/index.html"
+  },
+  {
+    "revision": "1bca3cb22cab778b7121",
+    "url": "/curriculo-vetta/static/css/main.2a716292.chunk.css"
+  },
+  {
+    "revision": "358357173de3782ed07f",
+    "url": "/curriculo-vetta/static/js/2.9a8d811b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/curriculo-vetta/static/js/2.9a8d811b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bca3cb22cab778b7121",
+    "url": "/curriculo-vetta/static/js/main.131b2403.chunk.js"
+  },
+  {
+    "revision": "ad94581966c5a45b23a4",
+    "url": "/curriculo-vetta/static/js/runtime-main.594fb7bc.js"
+  },
+  {
+    "revision": "ae594107ccc45c98b7bc37ede6b0f995",
+    "url": "/curriculo-vetta/static/media/foto.ae594107.png"
+  }
+]);
